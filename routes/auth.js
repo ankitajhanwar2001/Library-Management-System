2883a35efef23b940770539f9c47dd22342f1165
Ankita Jhanwar <ankitajhanwar991@gmail.com>
2022-07-10 22:43:42 +0530

    Library management system

---
+++ routes/auth.js
@@ -0,0 +1,65 @@
+const express = require("express"),
+      router = express.Router({mergeParams: true}),
+      passport = require('passport');
+
+
+// Import index controller
+const authController = require('../controllers/auth');
+
+// Import models
+const User = require("../models/user");
+
+//landing page
+router.get('/', authController.getLandingPage);
+
+//admin login handler
+router.get("/auth/admin-login", authController.getAdminLoginPage)
+
+router.post("/auth/admin-login", passport.authenticate("local", {
+        successRedirect : "/admin",
+        failureRedirect : "/auth/admin-login",
+    }), (req, res)=> {
+      console.log(req.body);
+});
+
+//admin logout handler
+router.get("/auth/admin-logout", function(req, res, next) {
+    req.logout(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+  });
+});
+
+
+// admin sign up handler
+router.get("/auth/admin-signup", authController.getAdminSignUp);
+
+router.post("/auth/admin-signup", authController.postAdminSignUp);
+
+//user login handler
+router.get("/auth/user-login", authController.getUserLoginPage);
+
+router.post("/auth/user-login", passport.authenticate("local", {
+        successRedirect : "/user/1",
+        failureRedirect : "/auth/user-login",
+    }), (req, res)=> {
+});
+
+//user -> user logout handler
+router.get("/auth/user-logout", function(req, res, next) {
+    req.logout(function(err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+  });
+});
+
+//user sign up handler
+router.get("/auth/user-signup", authController.getUserSignUp);
+
+router.post("/auth/user-signup", authController.postUserSignUp);
+
+module.exports = router;
